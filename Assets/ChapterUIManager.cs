1590d7a24864546fe6e060ab5fe7ae4983605436
ILHWANK <dodjungvv@naver.com>
2024-01-18 01:39:39 +0900

    Chapter1 Scene UI 수정
    Skill 및 Hp 등 디테일 추가

--- Assets/ChapterUIManager.cs
+++ Assets/ChapterUIManager.cs
@@ -7,17 +7,71 @@ using UnityEngine.SceneManagement;
 public class ChapterUIManager : MonoBehaviour
 {
     [SerializeField]
-    Button backButton;
+    Button backButton, skiil1Button, skill2Button, skill3Button, victoryExitButton, defeatRetryButton, defeatExitButton;
+
+    [SerializeField]
+    GameObject skill1Object, skill2Object, skill3Object, victoryObject, defeatObject;
 
     void Start()
     {
         // AddListener
         backButton.onClick.AddListener(OnClick_BackButton);
+        skiil1Button.onClick.AddListener(OnClick_Skill1Button);
+        victoryExitButton.onClick.AddListener(OnClick_VictoryExitButton);
+        defeatRetryButton.onClick.AddListener(OnClick_DefeatRetryButton);
+        defeatExitButton.onClick.AddListener(OnClick_DefeatExitButton);
+
     }
 
+    void TempExit()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    // Pnael
+    void EnableVictoryPanel(bool isEnable)
+    {
+        victoryObject.SetActive(isEnable);
+    }
+
+    void EnableDefeatPanel(bool isEnable)
+    {
+        defeatObject.SetActive(isEnable);
+    }
+
+    // OnClick
     void OnClick_BackButton()
     {
-        Debug.Log("확인용");
-        SceneManager.LoadScene("Main");
+        EnableDefeatPanel(true);
+    }
+
+    void OnClick_Skill1Button()
+    {
+        EnableVictoryPanel(true);
+    }
+
+    void OnClick_Skill2Button()
+    {
+
+    }
+
+    void OnClick_Skill3Button()
+    {
+
+    }
+
+    void OnClick_VictoryExitButton()
+    {
+        TempExit();
+    }
+
+    void OnClick_DefeatRetryButton()
+    {
+        SceneManager.LoadScene("Chapter1");
+    }
+
+    void OnClick_DefeatExitButton()
+    {
+        TempExit();
     }
 }
